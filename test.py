83ac8388167792ca650e50738a951afa38eb45c3
Arthur Dias <arthursantos01@gmail.com>
2022-11-19 12:34:25 -0300

    more solutions for challenges

---
+++ test.py
@@ -0,0 +1,67 @@
+# a = [0,9,8,7,6,5,4,3,2,1]
+# a[-1]
+# print(a[-1])
+
+# def a(b,c,d): pass
+
+# print(type(a))
+
+# print(a(1,2,3))
+
+# def a(): return 10;
+
+# print(type(a))
+
+# print(5//2)
+
+# a = { 1:9, 2:8, 3:7, 4:6, 5:5 }
+# print(a.items())
+
+# try:
+#   [1,2,3][4]
+# except IndexError:
+#   print("IndexError raised")
+# except:
+#   print("Error raised")
+# else:
+#   print("Something else happened")
+# finally:
+#   print("cleaning up")
+
+# a = 7
+# print(a.__str__())
+
+
+# print(set([1,2,1]) == set([1,2]))
+
+# a = {1:9 , 2:8 , 3:7 , 4:6 , 5:5}
+# print(a.get(6))
+
+# def f():
+#   f()
+#   return 42
+
+# print(f())
+
+# print "Hello" 'world'*2
+
+# class test():
+#   id = 0
+#   def __init__(self,id):
+#     self.id = id
+#     id = 2
+
+# t = test(1)
+# print(t.id)
+
+# a = [ 1, 2, 3, 4, 5 ]
+# print(sum(a))
+
+# print(filter(lambda x: x%2, [i for i in range(10)]))
+# print(map(lambda x: x%2, [i for i in range(10)]))
+# print(zip([i for i in range(10)], [i for i in range(10)]))
+
+# m = re.search(r'(ab[cd]?)', "acdeabdabcde")
+# print(m.groups())
+
+# print(sys.path)
